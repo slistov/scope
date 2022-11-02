44ef82f4925e551cdd3afec39054ca151843b7b7
Sergey <slistov@mail.ru>
2022-11-02 19:21:41 +0300

    small refact: model update

--- src/scope/domain/model.py
+++ src/scope/domain/model.py
@@ -1,6 +1,8 @@
 class Quote:
-    def __init__(self, movie, quote_eng, quote_rus) -> None:
+    def __init__(self, movie, speaker, quote_eng, quote_rus, stuff) -> None:
         self.movie = movie
+        self.speaker = speaker
         self.quote_eng = quote_eng
         self.quote_rus = quote_rus
+        self.stuff = stuff
         
