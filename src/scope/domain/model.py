a7b7003444bbe4854e39e833144d14f41012708c
Sergey <slistov@mail.ru>
2022-11-02 17:25:36 +0300

    requrements.txt and quote model

---
+++ src/scope/domain/model.py
@@ -0,0 +1,6 @@
+class Quote:
+    def __init__(self, movie, quote_eng, quote_rus) -> None:
+        self.movie = movie
+        self.quote_eng = quote_eng
+        self.quote_rus = quote_rus
+        
